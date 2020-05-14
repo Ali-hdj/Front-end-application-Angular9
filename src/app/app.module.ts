66b9c84663ffb671d99e0ddbb135b5f37508b5ca
Ali Hadj-Said <mralihadjsaid@gmail.com>
2020-05-15 00:09:12 +0200

    formulaire inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { SeConnecterComponent } from './accueil/se-connecter/se-connecter.component';
+import { InscriptionFormComponent } from './accueil/inscription-form/inscription-form.component';
+import { RechercherComponent } from './accueil/rechercher/rechercher.component';
+import { NavigationComponent } from './accueil/navigation/navigation.component';
+import { FormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    SeConnecterComponent,
+    InscriptionFormComponent,
+    RechercherComponent,
+    NavigationComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatProgressBarModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
