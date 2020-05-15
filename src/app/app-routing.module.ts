a0ea1e7e5e78e3e40fb4f7da64f620721358d665
Ali Hadj-Said <mralihadjsaid@gmail.com>
2020-05-15 20:43:33 +0200

    ajout des routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToutlespublicationsComponent } from './toutlespublications/toutlespublications.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'tout-les-publications' , component : ToutlespublicationsComponent },
+  {path : '' , component : AccueilComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
