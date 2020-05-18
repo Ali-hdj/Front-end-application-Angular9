fa61007d6e7d87b050e6d50c60cabe1c578a01da
Ali Hadj-Said <mralihadjsaid@gmail.com>
2020-05-18 22:51:46 +0200

    ajou des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,9 +23,14 @@ import { ProfileComponent } from './accueil-connexion/profile/profile.component'
 import { MessagesComponent } from './accueil-connexion/messages/messages.component';
 import { RendezVousComponent } from './accueil-connexion/rendez-vous/rendez-vous.component';
 import { AnnoncesComponent } from './accueil-connexion/annonces/annonces.component';
-import { VoisinsComponent } from './voisins/voisins.component';
+import { VoisinsComponent } from './accueil-connexion/voisins/voisins.component';
 import { ContratsComponent } from './accueil-connexion/contrats/contrats.component';
 import { MapComponent } from './map/map.component';
+import { MessageComponent } from './accueil-connexion/messages/message/message.component';
+import { UnRendezVousComponent } from './accueil-connexion/rendez-vous/un-rendez-vous/un-rendez-vous.component';
+import { UnContratComponent } from './accueil-connexion/contrats/un-contrat/un-contrat.component';
+import { UnVoisinComponent } from './accueil-connexion/voisins/un-voisin/un-voisin.component';
+import { UnePublicationComponent } from './accueil-connexion/annonces/une-publication/une-publication.component';
 
 
 @NgModule({
@@ -49,7 +54,12 @@ import { MapComponent } from './map/map.component';
     AnnoncesComponent,
     VoisinsComponent,
     ContratsComponent,
-    MapComponent
+    MapComponent,
+    MessageComponent,
+    UnRendezVousComponent,
+    UnContratComponent,
+    UnVoisinComponent,
+    UnePublicationComponent
    
   ],
   imports: [
